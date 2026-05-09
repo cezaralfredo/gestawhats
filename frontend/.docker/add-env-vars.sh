1677fd1e2da6ed3d7e5437d39f9092d9f46702ad
cezaralfredo <34128004+cezaralfredo@users.noreply.github.com>
2026-05-09 19:57:36 -0300

    Atualiza Node 14->18 e remove dockerize (incompatível ARM64)

--- frontend/.docker/add-env-vars.sh
+++ frontend/.docker/add-env-vars.sh
@@ -4,11 +4,30 @@ _writeFrontendEnvVars() {
     sed -i "s/<noscript id=\"env-insertion-point\"><\/noscript>/<script>var ENV=${ENV_JSON_ESCAPED}<\/script>/g" ${PUBLIC_HTML}index.html
 }
 
-_writeNginxEnvVars() {
-    dockerize -template /etc/nginx/conf.d/default.conf:/etc/nginx/conf.d/default.conf
+_writeBackendProxyConfig() {
+    if [ -n "$BACKEND_SERVER_NAME" ] && [ -n "$URL_BACKEND" ]; then
+        cat >> /etc/nginx/conf.d/default.conf <<EOF
+
+server {
+    listen 80;
+    server_name ${BACKEND_SERVER_NAME};
+
+    location / {
+        set \$backend_url http://${URL_BACKEND};
+        proxy_pass \$backend_url;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_cache_bypass \$http_upgrade;
+    }
+}
+EOF
+    fi
 }
 
 _writeFrontendEnvVars;
-_writeNginxEnvVars;
-
-exit 0
+_writeBackendProxyConfig;
